00cf36e41457d52265fab56ac81a4f658b7d97d7
kaironst <pedronovak4@gmail.com>
2025-10-07 21:30:13 -0300

    streamlined jstl config

--- lua/custom/plugins/lsp/jdtls-java.lua
+++ lua/custom/plugins/lsp/jdtls-java.lua
@@ -1,4 +1,4 @@
-vim.env.JDTLS_JVM_ARGS = '-javaagent:' .. vim.fn.expand '$HOME/.local/share/nvim/mason/packages/lombok-nightly/lombok.jar'
+vim.env.JDTLS_JVM_ARGS = '-javaagent:' .. vim.fn.expand '$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar'
 
 return {
   'mfussenegger/nvim-jdtls',
@@ -9,48 +9,62 @@ return {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'j-hui/fidget.nvim',
   },
-  config = function()
-    local home = os.getenv 'HOME'
-    local jdtls = require 'jdtls'
 
-    -- Configure workspace directory
+  config = function()
+    local jdtls = require 'jdtls'
+    local home = vim.fn.expand '$HOME'
+
+    -- Caminhos principais
+    local jdtls_path = home .. '/.local/share/nvim/mason/packages/jdtls'
+    local lombok_jar = home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar'
+
+    -- Workspace único por projeto
     local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
     local workspace_dir = home .. '/.cache/jdtls-workspace/' .. project_name
 
-    -- Path to jdtls installation (adjust if needed)
-    local jdtls_path = home .. '/.local/share/nvim/mason/packages/jdtls'
-
-    -- Path to lombok
-    local lombok_jar = home .. '/.local/share/nvim/mason/packages/lombok-nightly/lombok.jar'
-
-    -- Main jdtls command
+    -- Configuração base
     local config = {
       cmd = {
         'java',
         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
         '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xms1g',
+        '-Xmx2G',
+        '-javaagent:' .. lombok_jar,
         '-jar',
-        jdtls_path .. '/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+        vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
         '-configuration',
         jdtls_path .. '/config_linux',
         '-data',
         workspace_dir,
       },
-      root_dir = require('jdtls.setup').find_root { '.git', 'pom.xml', 'build.gradle', 'settings.gradle' },
+
+      root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+
       settings = {
         java = {
           signatureHelp = { enabled = true },
           contentProvider = { preferred = 'fernflower' },
+          configuration = {
+            runtimes = {
+              {
+                name = 'JavaSE-17',
+                path = '/usr/lib/jvm/java-17-openjdk',
+              },
+            },
+          },
         },
       },
+
       init_options = {
-        bundles = {
-          vim.fn.glob(lombok_jar),
-        },
+        bundles = {}, -- deixa vazio; Lombok é javaagent, não bundle
       },
     }
 
-    -- Start or attach jdtls when opening Java files
+    -- Inicia o JDTLS automaticamente em arquivos Java
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'java',
       callback = function()
