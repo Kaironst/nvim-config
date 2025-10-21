03b4ee49d5d5425a3475699bef7d8c369c5d148b
Kaironst <pedronovak4@gmail.com>
2025-10-21 11:55:23 -0300

    got rid of my whack ass config in favor of a new one that actually works shocker i know i know

--- lua/custom/plugins/lsp/jdtls-java.lua
+++ lua/custom/plugins/lsp/jdtls-java.lua
@@ -11,46 +11,47 @@ return {
   },
   config = function()
     local home = os.getenv 'HOME'
-    local jdtls = require 'jdtls'
-
-    -- Configure workspace directory
-    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-    local workspace_dir = home .. '/.cache/jdtls-workspace/' .. project_name
-
-    -- Path to jdtls installation (adjust if needed)
     local jdtls_path = home .. '/.local/share/nvim/mason/packages/jdtls'
+    local lombok_jar = jdtls_path .. '/lombok.jar'
+    local launcher_jar = vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar')
+    local workspace_dir = home .. '/.cache/jdtls-workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
-    -- Path to lombok
-    local lombok_jar = home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar'
-
-    -- Main jdtls command
-    local config = {
-      cmd = {
-        'java',
-        '-javaagent:' .. lombok_jar,
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-jar',
-        vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
-        '-configuration',
-        jdtls_path .. '/config_linux',
-        '-data',
-        workspace_dir,
-      },
-      root_dir = require('jdtls.setup').find_root { '.git', 'pom.xml', 'build.gradle', 'settings.gradle' },
-      settings = {
-        java = {
-          signatureHelp = { enabled = true },
-          contentProvider = { preferred = 'fernflower' },
-        },
-      },
+    local cmd = {
+      'java',
+      '-javaagent:' .. lombok_jar,
+      '-Dlombok.debug=true',
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Dlog.protocol=true',
+      '-Dlog.level=ALL',
+      '-Xms1g',
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens',
+      'java.base/java.util=ALL-UNNAMED',
+      '--add-opens',
+      'java.base/java.lang=ALL-UNNAMED',
+      '-jar',
+      launcher_jar,
+      '-configuration',
+      jdtls_path .. '/config_linux',
+      '-data',
+      workspace_dir,
     }
-
     -- Start or attach jdtls when opening Java files
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'java',
       callback = function()
-        jdtls.start_or_attach(config)
+        require('jdtls').start_or_attach {
+          cmd = cmd,
+          root_dir = require('jdtls.setup').find_root { '.git', 'pom.xml', 'build.gradle' },
+          settings = {
+            java = {
+              signatureHelp = { enabled = true },
+              contentProvider = { preferred = 'fernflower' },
+            },
+          },
+        }
       end,
     })
   end,
