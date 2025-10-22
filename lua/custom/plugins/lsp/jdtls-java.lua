e6248a296109757f1b7661cb59d73bf3a0f2e0b1
Kaironst <pedronovak4@gmail.com>
2025-10-22 15:15:14 -0300

    i don't even know what i did but i hope to got this fixes it

--- lua/custom/plugins/lsp/jdtls-java.lua
+++ lua/custom/plugins/lsp/jdtls-java.lua
@@ -1,58 +1,48 @@
-vim.env.JDTLS_JVM_ARGS = '-javaagent:' .. vim.fn.expand '$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar'
-
 return {
-  'mfussenegger/nvim-jdtls',
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    'j-hui/fidget.nvim',
-  },
-  config = function()
-    local home = os.getenv 'HOME'
-    local jdtls_path = home .. '/.local/share/nvim/mason/packages/jdtls'
-    local lombok_jar = jdtls_path .. '/lombok.jar'
-    local launcher_jar = vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar')
-    local workspace_dir = home .. '/.cache/jdtls-workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+  {
+    'mfussenegger/nvim-jdtls',
+    ft = { 'java' },
+    config = function()
+      local jdtls = require 'jdtls'
 
-    local cmd = {
-      'java',
-      '-javaagent:' .. lombok_jar,
-      '-Dlombok.debug=true',
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      '-Dosgi.bundles.defaultStartLevel=4',
-      '-Declipse.product=org.eclipse.jdt.ls.core.product',
-      '-Dlog.protocol=true',
-      '-Dlog.level=ALL',
-      '-Xms1g',
-      '--add-modules=ALL-SYSTEM',
-      '--add-opens',
-      'java.base/java.util=ALL-UNNAMED',
-      '--add-opens',
-      'java.base/java.lang=ALL-UNNAMED',
-      '-jar',
-      launcher_jar,
-      '-configuration',
-      jdtls_path .. '/config_linux',
-      '-data',
-      workspace_dir,
-    }
-    -- Start or attach jdtls when opening Java files
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'java',
-      callback = function()
-        require('jdtls').start_or_attach {
-          cmd = cmd,
-          root_dir = require('jdtls.setup').find_root { '.git', 'pom.xml', 'build.gradle' },
-          settings = {
-            java = {
-              signatureHelp = { enabled = true },
-              contentProvider = { preferred = 'fernflower' },
-            },
-          },
-        }
-      end,
-    })
-  end,
+      -- Mason paths
+      local mason_path = vim.fn.stdpath 'data' .. '/mason/packages/jdtls'
+      local lombok_path = mason_path .. '/lombok.jar'
+      local workspace_dir = vim.fn.stdpath 'cache' .. '/jdtls/workspace'
+
+      -- Find Eclipse launcher jar dynamically
+      local launcher_jar = vim.fn.glob(mason_path .. '/plugins/org.eclipse.equinox.launcher_*.jar')
+
+      local config = {
+        cmd = {
+          'java',
+          '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+          '-Dosgi.bundles.defaultStartLevel=4',
+          '-Declipse.product=org.eclipse.jdt.ls.core.product',
+          '-Dlog.level=ALL',
+          '-Xmx1G',
+          '--add-modules=ALL-SYSTEM',
+          '--add-opens',
+          'java.base/java.util=ALL-UNNAMED',
+          '--add-opens',
+          'java.base/java.lang=ALL-UNNAMED',
+          '-javaagent:' .. lombok_path,
+          '-Xbootclasspath/a:' .. lombok_path,
+          '-jar',
+          launcher_jar,
+          '-configuration',
+          mason_path .. '/config_linux', -- adapt for your OS
+          '-data',
+          workspace_dir,
+        },
+        root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'build.gradle', 'pom.xml' },
+        init_options = {
+          bundles = {},
+        },
+      }
+
+      -- Start or attach JDTLS
+      jdtls.start_or_attach(config)
+    end,
+  },
 }
--- lua/custom/plugins/lsp/main-lsp.lua
+++ lua/custom/plugins/lsp/main-lsp.lua
@@ -227,6 +227,11 @@ return {
       automatic_installation = false,
       handlers = {
         function(server_name)
+          -- dont autostart java
+          if server_name == 'jdtls' then
+            return
+          end
+
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
