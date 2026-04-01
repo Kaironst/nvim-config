fef971fde017ad89f3407c7a0ea6f88e6197ccbe
Kaironst <pedronovak4@gmail.com>
2026-04-01 20:29:56 -0300

    added barbar

---
+++ lua/custom/plugins/barbar.lua
@@ -0,0 +1,75 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+
+    -- Move to previous/next
+    map('n', '<leader>a.', '<Cmd>BufferPrevious<CR>', opts)
+    map('n', '<leader>a,', '<Cmd>BufferNext<CR>', opts)
+
+    -- Re-order to previous/next
+    map('n', '<leader>a<', '<Cmd>BufferMovePrevious<CR>', opts)
+    map('n', '<leader>a>', '<Cmd>BufferMoveNext<CR>', opts)
+
+    -- Goto buffer in position...
+    map('n', '<leader>a1', '<Cmd>BufferGoto 1<CR>', opts)
+    map('n', '<leader>a2', '<Cmd>BufferGoto 2<CR>', opts)
+    map('n', '<leader>a3', '<Cmd>BufferGoto 3<CR>', opts)
+    map('n', '<leader>a3', '<Cmd>BufferGoto 4<CR>', opts)
+    map('n', '<leader>a4', '<Cmd>BufferGoto 5<CR>', opts)
+    map('n', '<leader>a5', '<Cmd>BufferGoto 6<CR>', opts)
+    map('n', '<leader>a6', '<Cmd>BufferGoto 7<CR>', opts)
+    map('n', '<leader>a7', '<Cmd>BufferGoto 8<CR>', opts)
+    map('n', '<leader>a8', '<Cmd>BufferGoto 9<CR>', opts)
+    map('n', '<leader>a9', '<Cmd>BufferLast<CR>', opts)
+
+    -- Pin/unpin buffer
+    map('n', '<leader>ap', '<Cmd>BufferPin<CR>', opts)
+
+    -- Goto pinned/unpinned buffer
+    --                 :BufferGotoPinned
+    --                 :BufferGotoUnpinned
+
+    -- Close buffer
+    map('n', '<leader>ac', '<Cmd>BufferClose<CR>', opts)
+
+    -- Wipeout buffer
+    --                 :BufferWipeout
+
+    -- Close commands
+    --                 :BufferCloseAllButCurrent
+    --                 :BufferCloseAllButPinned
+    --                 :BufferCloseAllButCurrentOrPinned
+    --                 :BufferCloseBuffersLeft
+    --                 :BufferCloseBuffersRight
+
+    -- Magic buffer-picking mode
+    map('n', '<leader>aPp', '<Cmd>BufferPick<CR>', opts)
+    map('n', '<leader>aPd', '<Cmd>BufferPickDelete<CR>', opts)
+
+    -- Sort automatically by...
+    map('n', '<leader>ab', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+    map('n', '<leader>an', '<Cmd>BufferOrderByName<CR>', opts)
+    map('n', '<leader>ad', '<Cmd>BufferOrderByDirectory<CR>', opts)
+    map('n', '<leader>al', '<Cmd>BufferOrderByLanguage<CR>', opts)
+    map('n', '<leader>aw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+    -- Other:
+    -- :BarbarEnable - enables barbar (enabled by default)
+    -- :BarbarDisable - very bad command, should never be used
+  end,
+  opts = {
+    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    -- animation = true,
+    -- insert_at_start = true,
+    -- …etc.
+  },
+  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+}
